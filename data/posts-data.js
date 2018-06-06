65911cb81faddaf1a0412240f435ff9e0c80fdff
zx122248006 <zx122248006@qq.com>
2018-06-06 17:15:58 +0800

    修改数据文件

---
+++ data/posts-data.js
@@ -0,0 +1,28 @@
+var post_content = [{
+        date: "Sep 18 2016",
+        title: "一行白鹭上青天",
+        imgSrc: "/images/post/crab.png",
+        avatar: "/images/avatar/1.png",
+        content: "春花秋月何时了，往事知多少，小楼昨夜又东风，故国不堪回首月明中。雕栏玉砌应犹在，只是朱颜改，问君能有几多愁，恰似一江春水向东流。",
+        reading: "112",
+        collection: "96",
+    },
+    {
+        date: "NOV 23 2016",
+        title: "静夜思",
+        imgSrc: "/images/post/bl.png",
+        avatar: "/images/avatar/2.png",
+        content: "春花秋月何时了，往事知多少，小楼昨夜又东风，故国不堪回首月明中。雕栏玉砌应犹在，只是朱颜改，问君能有几多愁，恰似一江春水向东流。",
+        reading: "112",
+        collection: "96",
+    },
+    {
+        date: "NOV 23 2016",
+        title: "静夜思",
+        imgSrc: "/images/post/bl.png",
+        avatar: "/images/avatar/2.png",
+        content: "春花秋月何时了，往事知多少，小楼昨夜又东风，故国不堪回首月明中。雕栏玉砌应犹在，只是朱颜改，问君能有几多愁，恰似一江春水向东流。",
+        reading: "112",
+        collection: "96",
+    }
+]
