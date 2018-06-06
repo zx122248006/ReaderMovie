36111668f4c510da2531bba109b3e51adaa4f4d3
zx122248006 <zx122248006@qq.com>
2018-06-06 11:13:36 +0800

    使用数据绑定的形式，讲内容添加到页面中

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -5,70 +5,84 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var post_content1 = {
-      date:"Sep 18 2016",
-      title:"一行白鹭上青天",
-      post_img:"/images/post/crab.png",
-      content:"春花秋月何时了，往事知多少，小楼昨夜又东风，故国不堪回首月明中。雕栏玉砌应犹在，只是朱颜改，问君能有几多愁，恰似一江春水向东流。",
-      view_num:"112",
-      collect_num:"96",
-      author_img:"/images/avatar/1.png",
-    }
+    var post_content = [{
+        date: "Sep 18 2016",
+        title: "一行白鹭上青天",
+        post_img: "/images/post/crab.png",
+        content: "春花秋月何时了，往事知多少，小楼昨夜又东风，故国不堪回首月明中。雕栏玉砌应犹在，只是朱颜改，问君能有几多愁，恰似一江春水向东流。",
+        view_num: "112",
+        collect_num: "96",
+        author_img: "/images/avatar/1.png",
+      },
+      {
+        date: "NOV 23 2016",
+        title: "静夜思",
+        post_img: "/images/post/bl.png",
+        content: "春花秋月何时了，往事知多少，小楼昨夜又东风，故国不堪回首月明中。雕栏玉砌应犹在，只是朱颜改，问君能有几多愁，恰似一江春水向东流。",
+        view_num: "112",
+        collect_num: "96",
+        author_img: "/images/avatar/2.png",
+      }
+    ]
+    // 使用this.setData 绑定数据，然后再设置一个键名，使得在data：{}数据中，存在一个键名，在页面中使用wx:for="{{定义的键名}}"，进行循环。
+    this.setData({
+      posts_key: post_content
+    });
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
