7e613d6fc6f5ba8b912df7bd1f8ba964c76db65d
zx122248006 <zx122248006@qq.com>
2018-06-14 10:48:42 +0800

    创建模板文件

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -5,12 +5,8 @@ var postsData = require('../../data/posts-data.js');
 
 
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    
   },
 
   /**
@@ -19,6 +15,8 @@ Page({
   onLoad: function (options) {
 
     // 使用this.setData 绑定数据，然后再设置一个键名，使得在data：{}数据中，存在一个键名，在页面中使用wx:for="{{定义的键名}}"，进行循环。
+    // this.data 在122100版本之后已经失效，使用数据绑定时，需使用this.setData方法
+    // this.data.postList = postsData.postList;
     this.setData({
       posts_key: postsData.postList
     });
