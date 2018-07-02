c680e1099bc62ee2b540f9bce26ad344d30b9613
zx122248006 <zx122248006@qq.com>
2018-07-02 15:30:40 +0800

    修改部分内容

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -16,9 +16,11 @@ Page({
 
     // 使用this.setData 绑定数据，然后再设置一个键名，使得在data：{}数据中，存在一个键名，在页面中使用wx:for="{{定义的键名}}"，进行循环。
 
+
     // this.data 在122100版本之后已经失效，使用数据绑定时，需使用this.setData方法
 
     // this.data.postList = postsData.postList;
+    
     this.setData({
       posts_key: postsData.postList
     });
