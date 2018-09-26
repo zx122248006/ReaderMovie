e82ebf44d57301feb10690725c72365d26565fff
zx122248006 <zx122248006@qq.com>
2018-09-26 17:13:41 +0800

    设置缓存

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -3,6 +3,7 @@ var postsData = require('../../../data/posts-data.js');
 Page({
   onLoad: function (option) {
 
+    // 获取传入的值
     var postId = option.id;
     var postData = postsData.postList[postId];
 
@@ -16,6 +17,34 @@ Page({
       posts_detail: postData.detail
     });
 
+    //一个同步的缓存
+    // wx.setStorageSync('key',"风暴英雄")
+
+    // 修改缓存
+    wx.setStorageSync('key', {
+      game: '风暴英雄',
+      developer: '暴雪'
+    }),
+
+    wx.setStorageSync('key1', {
+      game: '绝地求生',
+      developer: '蓝洞'
+    })
+
   },
 
+  // 获取缓存
+  onCollectionTap: function (event) {
+    var game = wx.getStorageSync('key')
+    console.log(game)
+  },
+
+  onShareTap: function (event) {
+    
+    // 删除缓存
+    // wx.removeStorageSync('key')
+
+    // 删除所有缓存
+    wx.clearStorageSync()
+  }
 })
