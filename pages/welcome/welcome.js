33f95e0dedde15e49b558988da2fd152d964ede3
zx122248006 <zx122248006@qq.com>
2020-03-13 21:34:07 +0800

    更新学习

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -2,6 +2,11 @@ Page({
   onTap:function (){
     // console.log("hello")
     
+    // wx.navigateTo({
+    //   url: '../posts/post'
+    // })
+    // navigateTo 跳转之后，可以返回之前的页面
+
     wx.redirectTo({
       url: '../posts/post',
     })
