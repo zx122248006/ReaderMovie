9010f13566a37d3bd61fa738da618f92aba151e4
zx122248006 <zx122248006@qq.com>
2020-03-18 21:27:13 +0800

    更新小程序使用缓存来改变收藏的状态

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -25,47 +25,89 @@ Page({
     })
     // console.log(this.data)
 
+    // 获取缓存
+    let post = wx.getStorageSync('collected');
 
-    // wx.setStorageSync('key', {
-    //   game: '风暴英雄',
-    //   developer: 'ahahah'S
-
-    // });
-
-
-    let postsCollected = wx.getStorageSync('posts_Collected');
-    
-    if (postsCollected) {
-      let postCollected = postsCollected[postId]
-      if (postCollected) {
-
+    // 判断缓存是否存在
+    if (post) {
+      let p = post[postId];
+      // 判断当前文章是否存在缓存，如果存在，就将数据绑定到data中。
+      if (p) {
         this.setData({
-          collected: postCollected
+          collected: p
         })
       }
-
-
     } else {
-      let postsCollected = {};
-      postsCollected[postId] = false;
-      wx.setStorageSync('posts_Collected', postsCollected);
+      // 缓存不存在时，增加一个缓存
+      let posts = {};
+      posts[postId] = false;
+      wx.setStorageSync('collected', posts);
     }
+
+    // let postsCollected = wx.getStorageSync('posts_Collected');
+
+    // if (postsCollected) {
+    //   let postCollected = postsCollected[postId]
+    //   if (postCollected) {
+
+    //     this.setData({
+    //       collected: postCollected
+    //     })
+    //   }
+    // } else {
+    //   let postsCollected = {};
+    //   postsCollected[postId] = false;
+    //   wx.setStorageSync('posts_Collected', postsCollected);
+    // }
 
 
     console.log(this.data)
   },
 
+  // 点击收藏按钮时
   onCollectionTap: function (event) {
-    let postsCollected = wx.getStorageSync('posts_Collected');
-    let postCollected = postsCollected[this.data.postId]
-    postCollected = !postCollected;
+    // 获取缓存
+    let p = wx.getStorageSync('collected');
+    // console.log(p)
+    // 获取当前文章的缓存
+    let b = p[this.data.postId];
 
-    postsCollected[this.data.postId] = postCollected
-    wx.setStorageSync('posts_Collected', postsCollected)
-    console.log(this.data)
+    // 判断当前缓存是否存在，如果当前缓存不存在，就是非收藏状态，就将状态改为收藏 即 收藏状态 = true
+    if (b == undefined) {
+      b = true
+    } else {
+      // 当前缓存存在的话，就改为另外一个状态。
+      b = !b
+    }
+    // 设置 当前id的缓存 的值为 当前的状态
+    p[this.data.postId] = b;
+
+    // 设置缓存
+    wx.setStorageSync('collected', p)
+
+    console.log(b)
+    // 改变绑定的数据
     this.setData({
-      collected:postCollected
+      collected: b
     })
+
+
+
+
+
+
+    // let postsCollected = wx.getStorageSync('posts_Collected');
+    // let postCollected = postsCollected[this.data.postId]
+    // postCollected = !postCollected;
+
+    // postsCollected[this.data.postId] = postCollected
+    // wx.setStorageSync('posts_Collected', postsCollected)
+    // console.log(this.data)
+
+    // this.setData({
+    //   collected:postCollected
+    // })
+
   }
 
   // onCollectionTap: function (event) {
