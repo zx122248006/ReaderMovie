a10090aa4656e09b2fd77d2e7c05ceae234ca337
zx122248006 <zx122248006@qq.com>
2020-03-18 17:34:02 +0800

    更新学习。

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -1,5 +1,5 @@
 let postsData = require('../../../data/posts-data.js')
-let b ;
+
 Page({
 
   /**
@@ -15,27 +15,58 @@ Page({
   onLoad: function (event) {
     // 通过event 获取传递的值
     let postId = event.id;
-    b = postId;
     // 使用变量存储数据
     let postData = postsData.postList;
 
     // 将对应的数据绑定到data中
     this.setData({
-      postKey: postData[postId]
+      postKey: postData[postId],
+      postId
     })
     // console.log(this.data)
 
 
     // wx.setStorageSync('key', {
     //   game: '风暴英雄',
-    //   developer: 'ahahah'
+    //   developer: 'ahahah'S
 
     // });
 
 
+    let postsCollected = wx.getStorageSync('posts_Collected');
+    
+    if (postsCollected) {
+      let postCollected = postsCollected[postId]
+      if (postCollected) {
 
-    let postCollected 
+        this.setData({
+          collected: postCollected
+        })
+      }
+
+
+    } else {
+      let postsCollected = {};
+      postsCollected[postId] = false;
+      wx.setStorageSync('posts_Collected', postsCollected);
+    }
+
+
+    console.log(this.data)
   },
+
+  onCollectionTap: function (event) {
+    let postsCollected = wx.getStorageSync('posts_Collected');
+    let postCollected = postsCollected[this.data.postId]
+    postCollected = !postCollected;
+
+    postsCollected[this.data.postId] = postCollected
+    wx.setStorageSync('posts_Collected', postsCollected)
+    console.log(this.data)
+    this.setData({
+      collected:postCollected
+    })
+  }
 
   // onCollectionTap: function (event) {
   //   let game = wx.getStorageSync('key')
@@ -53,4 +84,3 @@ Page({
   // }
 
 })
-console.log(b)
